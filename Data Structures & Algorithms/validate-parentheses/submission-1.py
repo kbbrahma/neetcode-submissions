f25eb3409e13a66b3e37a81e9418403c8637f798
Kamal Bahadur <kamalcool.007@gmail.com>
2026-09-09 13:26:53 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+  def isValid(self, s: str) -> bool:
+    pmap = {')':'(', ']':'[', '}':'{'}
+    stack = []
+    for p in s:
+      if p not in pmap.keys():
+        stack.append(p)
+      else:
+        if len(stack) and pmap.get(p) == stack[-1]:
+          print("stack has: ",stack)
+          stack.pop()
+        else:
+          return False
+    if len(stack):
+      return False
+    else:
+      return True       
+
+        
